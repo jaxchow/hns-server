b3bfe49f2cfc591d22a7ecb8ce0266ad993bf2bd
Yingjj <yksdjx@126.com>
2016-01-04 20:38:45 +0800

    红包雨提交

--- js/wechat/module.redrian.js
+++ js/wechat/module.redrian.js
@@ -10,10 +10,47 @@
 require(['jquery'], function() {
 	//alert(12)
     $('.rain').on('click',function(){
-    	alert(12)
-    	//$('.rain').hide();
+    	$('#successModalBox').removeClass('hide');
+    	$('.rain').remove();
+    	$.ajax({
+			url: '/wechat/openpackage.do',
+			type: 'get',
+			dataType: 'json',
+			success: function(res) {
+				var giftNum=res.giftNum,
+					giftType=res.giftType;
+				$('.giftsp').text(giftNum+giftType);
+			}
+		});
     });
+    $('.togetBtn').on('click',function(e){
+    	$('#shareMatte').removeClass('hide');
+    })
+    function formate(d){
+		return d>9?d:'0'+d;
+	}
+    var timeSp=$(".time-down"),
+    	coolTime=parseInt(timeSp.data('time'),10),
+    	nowTime=new Date(),
+    	remaining=nowTime-coolTime;
 
-    coolTime=parInt($(".time-down").data('time'),10);
+    function showtime(remaining,timeSp){
+    	var mytime,
+    		h=Math.floor(remaining/1000/60/60),
+      		m=Math.floor(remaining/1000/60%60),
+       		s=Math.floor(remaining/1000%60);
+       	mytime=formate(h)+':'+formate(m)+':'+formate(s);
+       	timeSp.text(mytime)
+       	
+    }
+    showtime(remaining,timeSp);
+    var countdown=setInterval(function(){
+    	remaining=remaining-1000;
+    	showtime(remaining,timeSp);    	
+    	if(remaining<1000){
+    		clearInterval(countdown);
+    	}
+    	
+    },1000)
     // to do 实现倒计时 格式化后写回 time-down
 });
